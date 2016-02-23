642d4f62fca9663f0385c632b4b76c4298399258
Chris Brown <cfbrown956@gmail.com>
2016-02-23 17:13:04 +0000

    Added back button to settings page

--- app/src/main/java/com/cfbrownweb/fuelmemo/SettingsActivity.java
+++ app/src/main/java/com/cfbrownweb/fuelmemo/SettingsActivity.java
@@ -5,13 +5,17 @@
 package com.cfbrownweb.fuelmemo;
 
 import android.app.Dialog;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.NumberPicker;
@@ -33,6 +37,8 @@ public class SettingsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_settings);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         //Store all tile colour buttons
         tileColourButtons = new ArrayList<Button>();
@@ -174,6 +180,22 @@ public class SettingsActivity extends AppCompatActivity {
             view.setBackgroundDrawable(background);
         } else {
             view.setBackground(background);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        switch (id) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
     }
 
